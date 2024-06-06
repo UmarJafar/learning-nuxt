41ef91b3b9e201dd6f5528b2f4ad8c9a6ee76e83
Umar Jafar <umardev.seedout@gmail.com>
2024-06-06 14:53:02 +0500

    intial

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+      class="p-12 bg-gray-500 w-full h-full min-h-screen flex flex-col items-center"
+  >
+    <slot/>
+  </div>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+
+  <h1 class="bg-sky-500 hover:bg-sky-700"> Home </h1>
+
+</template>
